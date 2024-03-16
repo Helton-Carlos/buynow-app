be2e143c0bd3afadf514225b724ca9b6f7b5a7b6
Helton Brito <heltoncarlospa@hotmail.com>
2024-03-16 15:14:06 -0300

    fix: adjust cache

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -1,10 +1,16 @@
-import { clientsClaim } from 'workbox-core'
-import { ExpirationPlugin } from 'workbox-expiration'
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching'
-import { registerRoute } from 'workbox-routing'
-import { StaleWhileRevalidate } from 'workbox-strategies'
+/// <reference lib="WebWorker" />
+/// <reference types="vite/client" />
 
-clientsClaim()
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
+import { clientsClaim } from 'workbox-core'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { StaleWhileRevalidate } from 'workbox-strategies'
+import { ExpirationPlugin } from 'workbox-expiration'
+
+declare let self: ServiceWorkerGlobalScope
+
+precacheAndRoute(self.__WB_MANIFEST);
+
 declare global {
   interface Window {
     INJECT_MANIFEST_PLUGIN: { url: string; revision: string }[]
@@ -12,45 +18,40 @@ declare global {
   }
 }
 
-// Add all assets generated during build to the browser cache.
-precacheAndRoute(self.INJECT_MANIFEST_PLUGIN)
+cleanupOutdatedCaches();
 
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$')
+let allowlist: undefined | RegExp[]
+
+registerRoute(new NavigationRoute(
+  createHandlerBoundToURL('/'),
+  { allowlist },
+))
+
 registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
-  ({ request, url }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') {
-      return false
-    } // If this is a URL that starts with /_, skip.
-
-    if (url.pathname.startsWith('/_')) {
-      return false
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false
-    } // Return true to signal that we want to use the handler.
-
-    return true
-  },
-  createHandlerBoundToURL('/index.html')
-)
-
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
-registerRoute(
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as 
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.css'), 
   new StaleWhileRevalidate({
-    cacheName: 'images-teste',
+    cacheName: 'css-teste',
     plugins: [
       new ExpirationPlugin({ maxEntries: 50 }),
     ],
   })
 )
 
+registerRoute(
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png') && url.pathname.includes('init'),
+  new StaleWhileRevalidate({
+    cacheName: 'images-init',
+    plugins: [
+      new ExpirationPlugin({ maxEntries: 50, maxAgeSeconds: 120 }),
+    ],
+  })
+);
+
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting()
   }
 })
+
+self.skipWaiting();
+clientsClaim();
