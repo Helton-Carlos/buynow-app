8cf69378769473ff18bdeea9f70c1f505a76e181
Helton Brito <heltoncarlospa@hotmail.com>
2024-03-30 14:38:04 -0300

    fix: change path approve page

--- pages/approve/index.vue
+++ pages/approve/index.vue
@@ -2,25 +2,36 @@
 import iconSearch from '/icons/search.svg';
 import { useStatusStore } from '~/stores/sectors';
 import { useProductsStore } from '~/stores/products';
+import { useRouter, useRoute } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
 
 const { validationIndexedDBsectors } = useStatusStore();
 const { validationIndexedDBProducts } = useProductsStore();
 
 const search = ref<string>('');
 const sectors = ref<string>('');
-const total = ref<string>('3.000,22');
 
 function getSearch() {
-  let title = validationIndexedDBProducts().map((item: { title: any; }) => item.title);
+  let title = validationIndexedDBProducts().map(
+    (item: { title: any }) => item.title,
+  );
 
   return title.filter((item: string) =>
     item.toLowerCase().includes(search.value.toLowerCase()),
   );
 }
 
+function pageProducts(title: string) {
+  navigateTo('/products-registered/' + title);
+}
+
 const getProducts = computed<any>(() => {
   return getSearch().map((searchs: any) => {
-    return validationIndexedDBProducts().find((item: any) => item.title === searchs);
+    return validationIndexedDBProducts().find(
+      (item: any) => item.title === searchs,
+    );
   });
 });
 </script>
@@ -28,31 +39,24 @@ const getProducts = computed<any>(() => {
 <template>
   <div class="mx-auto max-w-[450px]">
     <div class="flex justify-end items-center relative">
-      <img 
-        class="absolute mr-2 w-4" 
-        :src="iconSearch" 
-        alt="Search Icon" 
-      />
+      <img class="absolute mr-2 w-4" :src="iconSearch" alt="Search Icon" />
 
-      <input 
+      <input
         class="input px-2"
-        type="text" 
-        name="search" 
+        type="text"
+        name="search"
         id="search"
         placeholder="Pesquisar"
         v-model="search"
       />
     </div>
-    
-    <div>  
-      <select
-        class="input"
-        v-model="sectors"
-      >
+
+    <div>
+      <select class="input" v-model="sectors">
         <option disabled value="">Escolha um setor</option>
-        <option 
+        <option
           class="capitalize"
-          v-for="check in validationIndexedDBsectors()" 
+          v-for="check in validationIndexedDBsectors()"
           :key="check"
         >
           {{ check }}
@@ -60,23 +64,18 @@ const getProducts = computed<any>(() => {
       </select>
     </div>
 
-    <p class="font-bold text-base pb-4">
-      Provados:
-    </p>
+    <p class="font-bold text-base pb-4">Produtos cadastrados:</p>
 
     <div>
-      <p class="font-bold text-purple">
-        $ {{ total }}
-      </p>
-
       <card
-        v-for="(product, index) in getProducts" 
+        v-for="(product, index) in getProducts"
         :key="index"
-        :image= "product.image"
-        :title= "product.title"
+        :image="product.image"
+        :title="product.title"
         :sector="product.sector"
         :amount="product.amount"
         :price="product.price"
+        @getProduct="pageProducts(product.title)"
       />
     </div>
   </div>
